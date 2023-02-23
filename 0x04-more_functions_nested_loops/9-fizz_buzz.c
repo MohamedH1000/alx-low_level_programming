e46acbeffe098cf3c1d09cf811361201e501c7bd
MohamedH1000 <mohammedhisham115@gmail.com>
2023-02-23 16:16:48 -0500

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
@@ -18,18 +22,18 @@ int main(void)
 		{
 			printf("Buzz");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
 		else
-		printf("%d", i);
+		{
+			printf("%d", i);
+		}
 
 		if (i != 100)
 		{
 			printf(" ");
 		}
 	}
+
 	printf("\n");
+
 	return (0);
 }
