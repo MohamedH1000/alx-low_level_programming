832770a54b447d1e36d1278b0a404b3ad34210cb
MohamedH1000 <mohammedhisham115@gmail.com>
2023-03-23 14:55:40 -0700

    0. Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,16 @@
+#ifndef VARIAFUNC
+#define VARIAFUNC
+
+#include <stdarg.h>
+#include <stdio.h>
+
+typedef struct args_t
+{
+	char *format;
+	void (*function)();
+}args_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
