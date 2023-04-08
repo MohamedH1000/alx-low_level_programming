14a43821f788a85a97ddcdf67f4c060b0b243fcc
MohamedH1000 <mohammedhisham115@gmail.com>
2023-04-08 13:26:19 -0700

    0. 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strlen - to calculate the string length
+ * @c : the char to be calculated
+ * return : the length in an integer form
+ */
+int _strlen(const char *c)
+{
+	int a;
+
+	for (a = 0; c[a] != '\0'; a++)
+	{
+		continue;
+	}
+	return (a);
+}
+unsigned int binary_to_uint(const char *b)
+{
+	int j;
+	unsigned int ln;
+	unsigned int c = 1;
+	unsigned int f = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	ln = _strlen(b);
+	for (j = ln - 1; j >= 0; j--)
+	{
+		if (b[j] != '0' && b[j] != '1')
+		{
+			return (0);
+		}
+		if (b[j] == '1')
+		{
+			f += c;
+		}
+		c = c * 2;
+	}
+	return (f);
+}
